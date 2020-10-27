63637e4917b1d085950fce55fd37fab23e607dd0
Azhari Marzan <azhari.marzan@gmail.com>
2020-10-27 10:14:49 +0700

    + base trend routes

---
+++ src/routes/trend.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const trendController = require('../controllers/trendController');
+const router = express.Router();
+
+router.post('/timeline', trendController.timeline);
+router.post('/daily', trendController.daily);
+router.post('/autocomplete', trendController.autocomplete);
+router.post('/region', trendController.region);
+router.post('/realtime', trendController.realtime);
+router.post('/related-queries', trendController.relatedQueries);
+router.post('/related-topics', trendController.relatedTopics);
