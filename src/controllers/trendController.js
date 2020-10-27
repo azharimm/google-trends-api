73aac7502f6303e11d47a3a40ba7f91ceaa1f009
Azhari Marzan <azhari.marzan@gmail.com>
2020-10-27 10:14:59 +0700

    + base trend controller

---
+++ src/controllers/trendController.js
@@ -0,0 +1,7 @@
+exports.timeline = async (req, res) => {}
+exports.daily = async (req, res) => {}
+exports.autocomplete = async (req, res) => {}
+exports.region = async (req, res) => {}
+exports.realtime = async (req, res) => {}
+exports.relatedQueries = async (req, res) => {}
+exports.relatedTopics = async (req, res) => {}
